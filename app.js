70da8edab63d81dbfebce2b8d4734a7b837384b8
Robinee <yb6925@naver.com>
2023-03-06 21:26:55 +0900

    Build render function and write some plan

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log('hello');
+const playboard = ['X', 'X', 'X', 'O', 'O', 'O', 'X', 'X', 'X'];
+
+//3. render playboard array(fill in the array with "x" and "o")
+const container = document.querySelector('.container');
+function render() {
+  //render block elements to container
+
+  //make block element
+  container.innerHTML = playboard
+    .map((block) => {
+      return `<div class="block">${block}</div>`;
+    })
+    .join('');
+
+  //put them into container
+}
+render();
+
+//4. build the function
+// -> allow players to add marks to a specific spot on the board
+// -> letting players click on the gameboard to place their mark
+//*keep players from playing in spots that are already taken
+
+//5. build the logic
+// -> check for when the game is over
+
+//6. clean up the interface
+// -> button to start / restart the game
+// -> display element that congradulate the wining player
