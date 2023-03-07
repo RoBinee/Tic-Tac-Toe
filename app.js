541635901848aa54bc5c379f7cec6bef9c8feff8
Robinee <yb6925@naver.com>
2023-03-07 21:49:49 +0900

    Erase all explainations and write Gameboard object

--- app.js
+++ app.js
@@ -1,29 +1,8 @@
-const playboard = ['X', 'X', 'X', 'O', 'O', 'O', 'X', 'X', 'X'];
+//test code for simple start
+const Gameboard = (() => {
+  const board = ['1', '2'];
 
-//3. render playboard array(fill in the array with "x" and "o")
-const container = document.querySelector('.container');
-function render() {
-  //render block elements to container
+  return { board };
+})();
 
-  //make block element
-  container.innerHTML = playboard
-    .map((block) => {
-      return `<div class="block">${block}</div>`;
-    })
-    .join('');
-
-  //put them into container
-}
-render();
-
-//4. build the function
-// -> allow players to add marks to a specific spot on the board
-// -> letting players click on the gameboard to place their mark
-//*keep players from playing in spots that are already taken
-
-//5. build the logic
-// -> check for when the game is over
-
-//6. clean up the interface
-// -> button to start / restart the game
-// -> display element that congradulate the wining player
+console.log(Gameboard);
