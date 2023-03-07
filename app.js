d01aea8a3ff0dc19922a9f0b1fb7bfe30109b51b
Robinee <yb6925@naver.com>
2023-03-07 22:09:23 +0900

    Build render function to show board on the screen

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 //test code for simple start
 const Gameboard = (() => {
   const board = ['1', '2'];
-
-  return { board };
 })();
 
-console.log(Gameboard);
+//Using Gameboard.board, make block element
+const container = document.querySelector('.container');
+function render() {
+  //render the contents of the gameboard array to the webpage
+
+  const blocks = Gameboard.board
+    .map((value) => {
+      return `<div class="block">${value}</div>`;
+    })
+    .join('');
+
+  //display blocks on screen
+  container.innerHTML = blocks;
+}
+render();
