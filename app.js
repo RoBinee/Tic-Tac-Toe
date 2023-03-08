93910713c9864d3330b37267b138808521ceafbb
Robinee <yb6925@naver.com>
2023-03-08 20:56:49 +0900

    Write 3 objects, gameBoard, displayController and player

---
+++ app.js
@@ -0,0 +1,13 @@
+const gameBoard = (() => {
+  //Module pattern -> use only once
+  const board = [];
+  return { board };
+})();
+
+const displayController = () => {
+  //Module pattern -> use only once
+};
+
+const player = () => {
+  //factory function pattern -> to use again
+};
