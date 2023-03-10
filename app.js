c3be4746b653e079da524a043e56157d19633a5e
Robinee <yb6925@naver.com>
2023-03-10 20:28:14 +0900

    Change obj names, DOM and game and Add build displayBoard func

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 const gameBoard = (() => {
   //Module pattern -> use only once
-  const board = [];
+
+  //create board
+  let board = ['X', 'X'];
+
   return { board };
 })();
 
-const displayController = () => {
+const DOM = (() => {
   //Module pattern -> use only once
-};
 
-const player = () => {
-  //factory function pattern -> to use again
+  const displayBoard = () => {
+    //**display gameBoard.board on the screen
+    const container = document.querySelector('.gameboard-container');
+
+    //format every board content
+    const board = gameBoard.board;
+    const formatted = board
+      .map((element) => {
+        return `<button class="block">${element}</button>`;
+      })
+      .join('');
+
+    //inset formatted element in the container
+    container.innerHTML = formatted;
+  };
+})();
+
+const game = () => {
+  console.log('game start');
 };
