ad6ffcf1ff5df09bb84a542188bd5a6f7525ac10
Robinee <yb6925@naver.com>
2023-03-11 14:21:10 +0900

    Write the code to add mark on the board

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const gameBoard = (() => {
 const DOM = (() => {
   //Module pattern -> use only once
 
+  //*** Display board function ***/
   const displayBoard = () => {
     //**display gameBoard.board on the screen
     const container = document.querySelector('.gameboard-container');
@@ -24,9 +25,35 @@ const DOM = (() => {
 
     //inset formatted element in the container
     container.innerHTML = formatted;
+    console.log('display Board work');
   };
-})();
 
+  displayBoard();
+
+  //*** Add mark function ***/
+  //make blockBtns clickable
+  const blockBtns = document.querySelectorAll('.block');
+  blockBtns.forEach((blockBtn, index) => {
+    //use index instead of id
+    blockBtn.addEventListener('click', () => {
+      //keeps players from playing in spots that are already taken
+      if (blockBtn.textContent === 'O') {
+        //if click btn is already taken, don't add mark
+        return;
+      }
+      console.log('add mark');
+      //change the value
+      blockBtn.textContent = 'O';
+      //change gameBoard.board too
+      //Using index, find blockBtn order in gameBoard.board
+      gameBoard.board[index] = 'O';
+
+      console.log(gameBoard.board);
+    });
+  });
+
+  // return { displayBoard };
+})();
 const game = () => {
   console.log('game start');
 };
